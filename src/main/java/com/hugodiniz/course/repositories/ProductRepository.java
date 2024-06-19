45cd3bb1159273bfe8ef8d66fbfcf8f0bfb0e090
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-06-19 12:25:51 -0300

    Product

---
+++ src/main/java/com/hugodiniz/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.hugodiniz.course.repositories;
+
+import com.hugodiniz.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
